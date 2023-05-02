bc98fce3f629c0dfd9d9edf6798550e43aa80b7b
Afthab <mohammedafthab15@gmail.com>
2023-05-02 11:50:54 +0530

    again

--- testingg/pgm_test.go
+++ testingg/pgm_test.go
@@ -17,7 +17,7 @@ func TestCreateTask(t *testing.T) {
 
 	// Define the expected insert query and result
 
-	expectedInsert := "INSERT INTO todo VALUES\\('test','50'\\);?"
+	expectedInsert := "INSERT INTO todo VALUES\\('test', '50'\\);?"
 
 	mock.ExpectExec(expectedInsert).WillReturnResult(sqlmock.NewResult(1, 1))
 
